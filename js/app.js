a29d0dd334870e8c5c895e1f2b06a27fceb72a34
Ignacio <ignacionazar9@gmail.com>
2025-05-26 00:01:36 -0300

    cree un juego, lo agregue en la coleccion y limpio el formulario

--- js/app.js
+++ js/app.js
@@ -8,14 +8,32 @@ const abrirModal = () => {
 const crearJuego = () => {
   //todo: tomar los datos del formulario y validarlos
   //con los datos voy a crear un objeto juego
-  const juegoNuevo = new Juego("Darth", "Vader", "3544664", "", "");
-  console.log(juegoNuevo);
+  const juegoNuevo = new Juego(
+    inputNombre.value,
+    inputGénero.value,
+    inputPlataforma.value,
+    inputDesarrollador.value,
+    inputImagen.value,
+    inputDescripción.value
+  );
   //guardar el juego en un array
+  coleccion.push(juegoNuevo);
+  limpiarFormulario();
+};
+
+const limpiarFormulario = () => {
+  formularioJuego.reset();
 };
 
 //declarar variables
 const btnAgregar = document.getElementById("btnAgregar");
 const formularioJuego = document.querySelector("form");
+const inputNombre = document.querySelector("#nombre");
+const inputGénero = document.querySelector("#género");
+const inputPlataforma = document.querySelector("#plataforma");
+const inputDesarrollador = document.querySelector("#desarrollador");
+const inputImagen = document.querySelector("#imagen");
+const inputDescripción = document.querySelector("#descripción");
 const coleccion = [];
 
 //agrego los manejadores de eventos
